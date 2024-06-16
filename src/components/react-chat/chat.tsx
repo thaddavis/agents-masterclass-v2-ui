34636b7e6abfae2d68c32142de37690c3dbbf09f
tad dy <a@a.com>
2024-06-16 13:32:10 -0400

    deploying website

--- src/components/react-chat/chat.tsx
+++ src/components/react-chat/chat.tsx
@@ -20,8 +20,6 @@ export function Chat({ id, className }: ChatProps) {
     scrollToBottom();
   }, [chatState.messages, scrollToBottom]);
 
-  console.log("chatState", chatState);
-
   return (
     <div
       className="group w-full overflow-auto pl-0 peer-[[data-state=open]]:lg:pl-[250px] peer-[[data-state=open]]:xl:pl-[300px]"
